0a3347cb8e123b4e52d49ba323d202c26e461fc5
Maarten de Graaf <wiejeben@gmail.com>
2017-06-27 21:24:11 +0200

    Add GPS data (unstable)

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ import urllib
 import urllib2
 from helpers import *
 
+gps_poller = GpsPoller()
+
 def app():
     threading.Timer(10.0, app).start()
+    global gps_poller
+    gps = gps_poller.gps_instance
 
     interface = "wlan0"
 
@@ -12,16 +16,25 @@ def app():
         'lan_ip': get_lan_ip(interface),
         'mac_address': get_mac_address(interface),
         'location': {
-            'lat': 0.0,
-            'long': 0.0
+            'lat': gps.fix.latitude,
+            'long': gps.fix.longitude
         }
     }
 
     # Send data to API
     reader = urllib2.urlopen("http://project.maarten.co.uk/test", urllib.urlencode(data))
 
-    # print reader.read()
+    print reader.read()
 
     reader.close()
 
-app()
+gps_poller.start()
+
+try:
+    app()
+except (KeyboardInterrupt, SystemExit):
+    print "\nKilling Thread..."
+    gps_poller.running = False
+    gps_poller.join()
+
+print "Done.\nExiting."
--- helpers.py
+++ helpers.py
@@ -1,6 +1,9 @@
 import socket
 import fcntl
 import struct
+import time
+import threading
+from gps import *
 
 def get_lan_ip(interface):
     try:
@@ -19,4 +22,20 @@ def get_mac_address(interface):
     except:
         str = "00:00:00:00:00:00" # default
 
-    return str[0:17]
+    return str[:17]
+
+
+# Threat that will poll the latest GPS info from the serial
+class GpsPoller(threading.Thread):
+	def __init__(self):
+		threading.Thread.__init__(self)
+		self.gps_instance = gps(mode=WATCH_ENABLE)
+		self.running = True
+
+	def run(self):
+		try:
+			while self.running:
+				self.gps_instance.next()
+				time.sleep(0.2)
+		except StopIteration:
+			pass
